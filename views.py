6524683feaafd79c5c47703d574f64e19f6d7d2b
dni ⚡ <office@dnilabs.com>
2024-07-25 09:28:38 +0200

    feat: cleanup code
    unused stuff

--- views.py
+++ views.py
@@ -1,15 +1,19 @@
-from fastapi import Depends, Request
+from fastapi import APIRouter, Depends, Request
 from fastapi.templating import Jinja2Templates
+from lnbits.helpers import template_renderer
 from lnbits.core.models import User
 from lnbits.decorators import check_user_exists
 from starlette.responses import HTMLResponse
 
-from . import decoder_ext, decoder_renderer
-
 templates = Jinja2Templates(directory="templates")
 
 
-@decoder_ext.get("/", response_class=HTMLResponse)
+def decoder_renderer():
+    return template_renderer(["decoder/templates"])
+
+decoder_generic_router: APIRouter = APIRouter()
+
+@decoder_generic_router.get("/", response_class=HTMLResponse)
 async def index(
     request: Request,
     user: User = Depends(check_user_exists),
