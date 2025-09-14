b3913ae0e1a6404aae13f72f4eea5312bd09b8a2
Arc <ben@arc.wales>
2025-09-14 18:03:45 +0100

    wrong import

--- views.py
+++ views.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter, Depends, Request
+from fastapi.responses import HTMLResponse
 from lnbits.core.models import User
 from lnbits.decorators import check_user_exists
 from lnbits.helpers import template_renderer
-from starlette.responses import HTMLResponse
-
 
 def decoder_renderer():
     return template_renderer(["decoder/templates"])
